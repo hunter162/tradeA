fae133fe8c1900eda781a3c7911eedfaa94ee26d
lane <daviego52010@gmail.com>
2025-02-21 02:10:43 +0800

    fixed bug

--- src/modules/services/tokenTradeService.js
+++ src/modules/services/tokenTradeService.js
@@ -2,7 +2,7 @@ import { logger } from '../utils/index.js';
 import db from '../db/index.js';
 import {Keypair, PublicKey} from '@solana/web3.js';
 import {CustomPumpSDK} from "./customPumpSDK.js";
-import {AnchorProvider, Wallet} from "@coral-xyz/anchor";
+import {AnchorProvider, BN, Wallet} from "@coral-xyz/anchor";
 
 export class TokenTradeService {
     constructor(solanaService, redisService) {
